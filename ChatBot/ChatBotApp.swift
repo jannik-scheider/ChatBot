37f16be563ff73b3f86fc02ca63eee827c0e90a6
Jannik Scheider <js509@hdm-stuttgart.de>
2024-09-14 15:38:26 +0200

    first commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Jannik Scheider on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
